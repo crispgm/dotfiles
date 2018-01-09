6c2d66380989d68bac7509c4d06ccd9e9e71960f
David Zhang <crispgm@gmail.com>
2018-01-09 21:46:54 +0800

    Passing args to method

--- snippets/deprecated_method.py
+++ snippets/deprecated_method.py
@@ -2,7 +2,7 @@
 
 def deprecated_method(replace_func):
     def make_deprecate(func):
-        def func_wrapper(self):
-            return getattr(self, replace_func)()
+        def func_wrapper(self, *args, **kwargs):
+            return getattr(self, replace_func)(*args, **kwargs)
         return func_wrapper
     return make_deprecate
