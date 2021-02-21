ab99a420965bd57f3fccec76a4638e585cb561ca
David Zhang <crispgm@gmail.com>
2021-02-21 19:44:34 +0800

    feat(nvim): minor enhancements

--- nvim/lua/config/mappings.lua
+++ nvim/lua/config/mappings.lua
@@ -14,19 +14,20 @@ set_keymap('n', '<leader>n', '<cmd>set nonumber norelativenumber<cr>')
 set_keymap('n', '<leader>N', '<cmd>set number<cr>')
 set_keymap('n', '<leader>R', '<cmd>set relativenumber<cr>')
 -- moving
-set_keymap('i', '<C-A>', '<Home>')
-set_keymap('i', '<C-E>', '<End>')
+set_keymap('i', '<c-a>', '<Home>')
+set_keymap('i', '<c-e>', '<End>')
 set_keymap('n', 'k', 'gk')
 set_keymap('n', 'j', 'gj')
+-- editing
+set_keymap('n', '<a-Up>', '<cmd>m .-2<cr>')
+set_keymap('n', '<a-Down>', '<cmd>m .+1<cr>')
+set_keymap('i', '<c-d>', '<esc>ddi')
 -- splits
 set_keymap('n', '<leader>s', '<c-w>w')
 set_keymap('n', '<c-j>', '<c-w>j')
 set_keymap('n', '<c-k>', '<c-w>k')
 set_keymap('n', '<c-h>', '<c-w>h')
 set_keymap('n', '<c-l>', '<c-w>l')
--- swap lines
-set_keymap('n', '<A-Up>', '<cmd>m .-2<cr>')
-set_keymap('n', '<A-Down>', '<cmd>m .+1<cr>')
 -- quickfix
 set_keymap('n', '<leader>cc', '<cmd>cclose<cr>')
 set_keymap('n', '<leader>;', '<cmd>cprev<cr>')
@@ -46,9 +47,14 @@ set_keymap('n', '<leader>7', '7gt')
 set_keymap('n', '<leader>8', '8gt')
 set_keymap('n', '<leader>9', '9gt')
 set_keymap('n', '<leader>0', '<cmd>tablast<cr>')
+-- buf
+set_keymap('n', '<leader>[', 'bprev')
+set_keymap('n', '<leader>]', 'bnext')
+-- command
+set_keymap('c', '<c-a>', '<Home>')
+set_keymap('c', '<c-e>', '<End>')
 -- others
-set_keymap('i', '\\\\', '<esc>')
-set_keymap('i', '<c-d>', '<esc>ddi')
+set_keymap('i', '\\]', '<esc>')
 
 -- LSP
 set_keymap('n', '<leader>ld', '<cmd>lua vim.lsp.buf.definition()<cr>')
@@ -73,11 +79,9 @@ set_keymap('n', '<leader>pi', '<cmd>PlugInstall<cr>')
 set_keymap('n', '<leader>pc', '<cmd>PlugClean<cr>')
 
 -- kommentary
-vim.api.nvim_set_keymap('n', '<C-_>', '<Plug>kommentary_line_default', {})
-vim.api.nvim_set_keymap('v', '<C-_>', '<Plug>kommentary_visual_default', {})
+vim.api.nvim_set_keymap('i', '<c-_>', '<Esc><Plug>kommentary_line_default', {})
+vim.api.nvim_set_keymap('n', '<c-_>', '<Plug>kommentary_line_default', {})
+vim.api.nvim_set_keymap('v', '<c-_>', '<Plug>kommentary_visual_default', {})
 
 -- git-blame
 set_keymap('n', '<leader>gb', '<cmd>GitBlameToggle<cr>')
-
--- goyo
-set_keymap('n', '<leader>go', '<cmd>Goyo<cr>')
