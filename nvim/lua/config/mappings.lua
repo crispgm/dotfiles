6a66cf1494760ef87fc0c217f12dd0b5b36dacbd
David Zhang <crispgm@gmail.com>
2021-03-10 18:18:51 +0800

    feat(nvim): remap lsp features

--- nvim/lua/config/mappings.lua
+++ nvim/lua/config/mappings.lua
@@ -60,14 +60,15 @@ set_keymap('i', '\\]', '<esc>')
 
 -- LSP
 set_keymap('n', '<leader>ld', '<cmd>lua vim.lsp.buf.definition()<cr>')
-set_keymap('n', '<leader>lh', '<cmd>lua vim.lsp.buf.hover()<cr>')
-set_keymap('n', '<leader>li', '<cmd>lua vim.lsp.buf.implementation()<cr>')
-set_keymap('n', '<leader>ls', '<cmd>lua vim.lsp.buf.signature_help()<cr>')
+set_keymap('n', '<leader>lD', '<cmd>lua vim.lsp.buf.declaration()<cr>')
 set_keymap('n', '<leader>lt', '<cmd>lua vim.lsp.buf.type_definition()<cr>')
+set_keymap('n', '<leader>li', '<cmd>lua vim.lsp.buf.implementation()<cr>')
+set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>')
+set_keymap('n', 'U', '<cmd>lua vim.lsp.buf.signature_help()<cr>')
 set_keymap('n', '<leader>lr', '<cmd>lua vim.lsp.buf.references()<cr>')
-set_keymap('n', '<leader>lsd', '<cmd>lua vim.lsp.buf.document_symbol()<cr>')
-set_keymap('n', '<leader>lsw', '<cmd>lua vim.lsp.buf.workspace_symbol()<cr>')
-set_keymap('n', '<leader>le', '<cmd>lua vim.lsp.buf.declaration()<cr>')
+set_keymap('n', '<leader>ls', '<cmd>lua vim.lsp.buf.document_symbol()<cr>')
+set_keymap('n', '<leader>lS', '<cmd>lua vim.lsp.buf.workspace_symbol()<cr>')
+set_keymap('n', '<leader>lR', '<cmd>lua vim.lsp.buf.rename()<cr>')
 set_keymap('n', '<leader>lf', '<cmd>lua vim.lsp.buf.formatting()<cr>')
 
 -- telescope
