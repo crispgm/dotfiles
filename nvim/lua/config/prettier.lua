4a6e427016da39d521f96b78dde1ea7798c4f476
David Zhang <crispgm@gmail.com>
2021-04-08 17:15:41 +0800

    fix(nvim): get pretter path with shell cmd

--- nvim/lua/config/prettier.lua
+++ nvim/lua/config/prettier.lua
@@ -1,4 +1,4 @@
 local vim = vim
 vim.g['prettier#autoformat'] = 1
 vim.g['prettier#autoformat_require_pragma'] = 0
-vim.g['prettier#exec_cmd_path'] = '/opt/homebrew/bin/prettier'
+vim.g['prettier#exec_cmd_path'] = vim.fn.system('which prettier')
