b9e1ada1385039532bff185aa76a2aff3e7077be
David Zhang <crispgm@gmail.com>
2021-05-21 11:49:15 +0800

    feat(nvim): esc to quit telescope [ci skip]

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -9,3 +9,14 @@ set_keymap('n', '<leader>fb', '<cmd>Telescope buffers<cr>')
 set_keymap('n', '<leader>fh', '<cmd>Telescope help_tags<cr>')
 set_keymap('n', '<leader>fk', '<cmd>Telescope keymaps<cr>')
 set_keymap('n', '<leader>fm', '<cmd>Telescope heading<cr>')
+
+local actions = require('telescope.actions')
+require('telescope').setup{
+    defaults = {
+        mappings = {
+            i = {
+                ['<esc>'] = actions.close
+            },
+        },
+    }
+}
