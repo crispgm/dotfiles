31db7a39be27fa240f680bb849b7da25891dd9b7
David Zhang <crispgm@gmail.com>
2021-05-29 21:38:05 +0800

    refactor(nvim): update to vim.opt

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -1,58 +1,57 @@
-local vim = vim
-local set_options = require('common').set_options
+local cmd = vim.cmd
+local opt = vim.opt
+local has = vim.fn.has
 
-vim.cmd('filetype plugin indent on')
-vim.cmd('syntax enable')
-set_options({
-    -- systematic
-    encoding = 'utf-8',
-    fileencoding = 'utf-8',
-    fileencodings = 'utf-8',
-    backup = false,     -- no .bak
-    swapfile = false,   -- no .swap
-    undofile = true,    -- use undo file
-    updatetime = 300,   -- time (in ms) to write to swap file
-    -- buffer
-    expandtab = true,   -- expand tab
-    tabstop = 4,        -- tab stop
-    softtabstop = 4,    -- soft tab stop
-    autoindent = true,  -- auto indent for new line
-    shiftwidth = 4,     -- auto indent shift width
-    -- window
-    number = true,
-    relativenumber = true,
-    -- editing
-    whichwrap = 'b,s,<,>,[,]', -- cursor is able to move from end of line to next line
-    backspace = 2,             -- backspace behaviors, is equivalent to indent,eol,start
-    list = true,               -- show tabs with listchars
-    ignorecase = false,        -- search with no ignore case
-    hlsearch = true,           -- highlight search
-    incsearch = false,         -- no incremental search
-    inccommand = 'nosplit',    -- live substitute preview
-    completeopt = 'menuone,noselect',
-    hidden = true,
-    cursorline = true,         -- show cursor line
-    ruler  = true,             -- show ruler line
-    colorcolumn = 120,         -- display a color column when line is longer than 120 chars
-    signcolumn = 'yes',        -- show sign column (column of the line number)
-    mouse = 'nv',              -- enable mouse under normal and visual mode
-    mousehide = true,          -- hide mouse when characters are typed
-    showmatch = true,          -- show bracket match
-    cmdheight = 2,             -- height of :command line
-    wildmenu = true,           -- wildmenu, auto complete for commands
-    wildmode = 'longest,full',
-    splitright = true,         -- split to right
-    splitbelow = true,         -- split to below
+cmd('filetype plugin indent on')
+cmd('syntax enable')
 
-})
-vim.cmd('set shortmess+=c')    -- status line e.g. CTRL+G
+-- systematic
+opt.encoding = 'utf-8'
+opt.fileencoding = 'utf-8'
+opt.fileencodings = {'utf-8'}
+opt.backup = false     -- no .bak
+opt.swapfile = false   -- no .swap
+opt.undofile = true    -- use undo file
+opt.updatetime = 300   -- time (in ms) to write to swap file
+-- buffer
+opt.expandtab = true   -- expand tab
+opt.tabstop = 4        -- tab stop
+opt.softtabstop = 4    -- soft tab stop
+opt.autoindent = true  -- auto indent for new line
+opt.shiftwidth = 4     -- auto indent shift width
+-- window
+opt.number = true
+opt.relativenumber = true
+-- editing
+opt.whichwrap = 'b,s,<,>,[,]' -- cursor is able to move from end of line to next line
+opt.backspace = {'indent', 'eol', 'start'} -- backspace behaviors
+opt.list = true               -- show tabs with listchars
+opt.ignorecase = false        -- search with no ignore case
+opt.hlsearch = true           -- highlight search
+opt.incsearch = false         -- no incremental search
+opt.inccommand = 'nosplit'    -- live substitute preview
+opt.completeopt = {'menuone', 'noselect'}
+opt.hidden = true
+opt.cursorline = true         -- show cursor line
+opt.ruler = true              -- show ruler line
+opt.colorcolumn = {120}       -- display a color column when line is longer than 120 chars
+opt.signcolumn = 'yes'        -- show sign column (column of the line number)
+opt.mouse = 'nv'              -- enable mouse under normal and visual mode
+cmd('set mousehide')          -- hide mouse when characters are typed
+opt.showmatch = true          -- show bracket match
+opt.cmdheight = 2             -- height of :command line
+opt.wildmenu = true           -- wildmenu, auto complete for commands
+opt.wildmode = {'longest', 'full'}
+opt.splitright = true         -- split to right
+opt.splitbelow = true         -- split to below
+opt.shortmess:append('c')     -- status line e.g. CTRL+G
 
-if not vim.fn.has('gui_running') then
-    vim.cmd('set t_Co=256')
+if not has('gui_running') then
+    opt.t_Co = 256
 end
-vim.cmd('set background=dark')
-if vim.fn.has('termguicolors') then
-    vim.cmd('let &t_8f = "\\<Esc>[38;2;%lu;%lu;%lum"')
-    vim.cmd('let &t_8b = "\\<Esc>[48;2;%lu;%lu;%lum"')
-    vim.cmd('set termguicolors')
+opt.background = 'dark'
+if has('termguicolors') then
+    cmd('let &t_8f = "\\<Esc>[38;2;%lu;%lu;%lum"')
+    cmd('let &t_8b = "\\<Esc>[48;2;%lu;%lu;%lum"')
+    opt.termguicolors = true
 end
