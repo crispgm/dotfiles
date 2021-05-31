bc187f9dba3506e6f73cb1901cecf7ed33e552aa
David Zhang <crispgm@gmail.com>
2021-05-31 17:26:57 +0800

    feat(nvim): migrate to vim.opt [ci skip] (#9)
    * refactor(nvim): update to vim.opt
    
    * chore(nvim): change comment style

--- nvim/lua/common.lua
+++ nvim/lua/common.lua
@@ -10,16 +10,4 @@ function M.nvim_set_keymap(mode, from, to, opts)
     vim.api.nvim_set_keymap(mode, from, to, opts)
 end
 
-function M.set_options(options)
-    for key, val in pairs(options) do
-        if val == true then
-            vim.api.nvim_command('set ' .. key)
-        elseif val == false then
-            vim.api.nvim_command('set no' .. key)
-        else
-            vim.api.nvim_command('set ' .. key .. '=' .. val)
-        end
-    end
-end
-
 return M
