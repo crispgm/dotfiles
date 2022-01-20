f6580e0317ab6efd6dad231930c7e8590f1d9b19
David Zhang <crispgm@gmail.com>
2022-01-20 16:22:59 +0800

    refactor(nvim): add map funcs [ci skip]

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -2,19 +2,18 @@ require('telescope').load_extension('fzf')
 require('telescope').load_extension('session-lens')
 require('telescope').load_extension('heading')
 
-local set_keymap = require('../common').set_keymap
-set_keymap(
-    'n',
+local nnoremap = require('../common').nnoremap
+nnoremap(
     '<leader>ff',
     '<cmd>Telescope find_files find_command=fd,--hidden,--no-ignore,--exclude,*.git,--type,f<cr>'
 )
-set_keymap('n', '<leader>fd', '<cmd>Telescope git_files<cr>')
-set_keymap('n', '<leader>fg', '<cmd>Telescope live_grep<cr>')
-set_keymap('n', '<leader>fb', '<cmd>Telescope buffers<cr>')
-set_keymap('n', '<leader>fh', '<cmd>Telescope help_tags<cr>')
-set_keymap('n', '<leader>fl', '<cmd>Telescope lsp_document_symbols<cr>')
-set_keymap('n', '<leader>fk', '<cmd>Telescope keymaps<cr>')
-set_keymap('n', '<leader>fm', '<cmd>Telescope heading<cr>')
+nnoremap('<leader>fd', '<cmd>Telescope git_files<cr>')
+nnoremap('<leader>fg', '<cmd>Telescope live_grep<cr>')
+nnoremap('<leader>fb', '<cmd>Telescope buffers<cr>')
+nnoremap('<leader>fh', '<cmd>Telescope help_tags<cr>')
+nnoremap('<leader>fl', '<cmd>Telescope lsp_document_symbols<cr>')
+nnoremap('<leader>fk', '<cmd>Telescope keymaps<cr>')
+nnoremap('<leader>fm', '<cmd>Telescope heading<cr>')
 
 local actions = require('telescope.actions')
 require('telescope').setup({
