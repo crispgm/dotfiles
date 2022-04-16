facc963cf804df1c327bf92b91e6e0523ed85f75
David Zhang <crispgm@gmail.com>
2022-04-16 13:00:17 +0800

    fix(nvim-cmp): add preset mappings

--- rc/config/nvim/lua/plugins/cmp.lua
+++ rc/config/nvim/lua/plugins/cmp.lua
@@ -1,8 +1,6 @@
 local cmp = require('cmp')
 cmp.setup({
-    mapping = {
-        ['<C-p>'] = cmp.mapping.select_prev_item(),
-        ['<C-n>'] = cmp.mapping.select_next_item(),
+    mapping = cmp.mapping.preset.insert({
         ['<C-d>'] = cmp.mapping.scroll_docs(-4),
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
         ['<C-Space>'] = cmp.mapping.complete(),
@@ -11,8 +9,8 @@ cmp.setup({
             behavior = cmp.ConfirmBehavior.Insert,
             select = true,
         }),
-    },
-    sources = {
+    }),
+    sources = cmp.config.sources({
         { name = 'nvim_lsp' },
         { name = 'nvim_lua' },
         { name = 'buffer' },
@@ -20,7 +18,7 @@ cmp.setup({
         { name = 'path' },
         { name = 'calc' },
         { name = 'emoji' },
-    },
+    }),
     snippet = {
         expand = function(args)
             vim.fn['vsnip#anonymous'](args.body)
