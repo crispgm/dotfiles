f31bbe40f5fd53724644ab18186cac118aa24be3
David Zhang <crispgm@gmail.com>
2022-04-16 12:20:23 +0800

    refactor(nvim): switch to vim.keymap api

--- rc/config/nvim/lua/common.lua
+++ rc/config/nvim/lua/common.lua
@@ -8,41 +8,37 @@ function M.try_require(name)
             'The configuration is not fully loaded. Requiring `%s` failed. Check the path and syntax.',
             name
         )
-        vim.api.nvim_echo(
-            { { 'init.lua', 'ErrorMsg' }, { ' ' .. msg } },
-            true,
-            {}
-        )
+        vim.notify(msg, vim.log.levels.ERROR)
     end
 end
 
-function M.map(mode, from, to, opts)
-    vim.api.nvim_set_keymap(mode, from, to, opts or {})
+function M.map(mode, lhs, rhs, opts)
+    vim.keymap.set(mode, lhs, rhs, opts or {})
 end
 
-function M.noremap(mode, from, to)
+function M.noremap(mode, lhs, rhs)
     local opts = { noremap = true }
-    vim.api.nvim_set_keymap(mode, from, to, opts)
+    vim.keymap.set(mode, lhs, rhs, opts)
 end
 
-function M.nmap(from, to)
-    vim.api.nvim_set_keymap('n', from, to)
+function M.nmap(lhs, rhs)
+    vim.keymap.set('n', lhs, rhs)
 end
 
-function M.nnoremap(from, to)
-    M.noremap('n', from, to)
+function M.nnoremap(lhs, rhs)
+    M.noremap('n', lhs, rhs)
 end
 
-function M.inoremap(from, to)
-    M.noremap('i', from, to)
+function M.inoremap(lhs, rhs)
+    M.noremap('i', lhs, rhs)
 end
 
-function M.vnoremap(from, to)
-    M.noremap('v', from, to)
+function M.vnoremap(lhs, rhs)
+    M.noremap('v', lhs, rhs)
 end
 
-function M.cnoremap(from, to)
-    M.noremap('c', from, to)
+function M.cnoremap(lhs, rhs)
+    M.noremap('c', lhs, rhs)
 end
 
 return M
