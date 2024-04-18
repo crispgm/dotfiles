16a3617ef2acce139d7e5159ac6dfdcc4b509c43
David Zhang <crispgm@gmail.com>
2024-04-18 10:58:06 +0800

    feat(nvim): finder default ignore [ci skip]

--- rc/config/nvim/lua/finder.lua
+++ rc/config/nvim/lua/finder.lua
@@ -21,6 +21,10 @@ require('telescope').load_extension('heading')
 
 nnoremap(
     '<leader>ff',
+    '<cmd>Telescope find_files find_command=fd,--hidden,--exclude,*.git,--type,f<cr>'
+)
+nnoremap(
+    '<leader>fF',
     '<cmd>Telescope find_files find_command=fd,--hidden,--no-ignore,--exclude,*.git,--type,f<cr>'
 )
 nnoremap('<leader>fd', '<cmd>Telescope git_files<cr>')
