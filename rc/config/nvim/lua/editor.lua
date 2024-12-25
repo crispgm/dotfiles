cd60ec74c7dac45c3870e27d7cadf183ce641513
David Zhang <crispgm@gmail.com>
2024-12-25 21:19:26 +0800

    fix(nvim): gitsigns warnings [ci skip]

--- rc/config/nvim/lua/editor.lua
+++ rc/config/nvim/lua/editor.lua
@@ -1,9 +1,9 @@
 require('gitsigns').setup({
     signs = {
-        add = { hl = 'GitGutterAdd', text = '+' },
-        change = { hl = 'GitGutterChange', text = '~' },
-        delete = { hl = 'GitGutterDelete', text = '_' },
-        topdelete = { hl = 'GitGutterDelete', text = '‾' },
-        changedelete = { hl = 'GitGutterChange', text = '~' },
+        add = { text = '+' },
+        change = { text = '~' },
+        delete = { text = '_' },
+        topdelete = { text = '‾' },
+        changedelete = { text = '~' },
     },
 })
